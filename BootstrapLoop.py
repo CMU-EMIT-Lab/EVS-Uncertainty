3e93c773db792bc229c9fd39b6dfb83d2909c1f5
jpminercmu <118566811+jpminercmu@users.noreply.github.com>
2024-08-06 09:40:03 -0400

    Add files via upload
    Modified column changes based on uploaded data in pandas row calls.

--- BootstrapLoop.py
+++ BootstrapLoop.py
@@ -16,13 +16,13 @@ except Exception:
     pass
 
 
-v1s = [395.84] #extrapolated volumes
+v1s = [250, 500, 1000] #extrapolated volumes
 
 dx = 0.001 #resolution of CDF
 
-df = pd.read_csv('parameters.csv') #read in csv of thresholds
+df = pd.read_csv('data/parameters.csv') #read in csv of thresholds
 
-poredf = pd.read_csv('poredf.csv')
+poredf = pd.read_csv('data/poredf.csv')
 
 
 #moving average for the PDF
@@ -88,132 +88,133 @@ def F_x(xs, l, conv = 0.0005):
     return rls.get()
 
 #loops over all volumes
-for v1 in v1s:
-    #loops over all samples
-    for i in range(len(df)):
-        row = df.iloc[i]
+with cp.cuda.Device(1):
+    for v1 in v1s:
+        #loops over all samples
+        for i in range(len(df)):
+            row = df.iloc[i]
 
-        pores = poredf[poredf.Name == row.Name]
-        evd = pores[pores['ESD']>row['Threshold (um)']]['ESD']
+            pores = poredf[poredf.Name == row.Name]
+            evd = pores[pores['Equivalent Spherical Diameter (um)']>row['Threshold (um)']]['Equivalent Spherical Diameter (um)']
 
-        dmax = np.max(evd)
-        
-        #https://bpb-us-w2.wpmucdn.com/sites.uwm.edu/dist/2/109/files/2016/04/2009_IME_final-1mmannh.pdf
-        threshold = row['Threshold (um)']
+            dmax = np.max(evd)
+            
+            #https://bpb-us-w2.wpmucdn.com/sites.uwm.edu/dist/2/109/files/2016/04/2009_IME_final-1mmannh.pdf
+            threshold = row['Threshold (um)']
 
-        #Check for missing threshold
-        if threshold == 0:
-            continue
+            #Check for missing threshold
+            if threshold == 0:
+                continue
 
-        #MOM Fit
-        scale = 0.5 * (evd.mean() - threshold) * ((evd.mean() - threshold)**2/evd.var()+1)
-        shape = -0.5 * ((evd.mean()-threshold)**2/(evd.var())-1)
+            #MOM Fit
+            scale = 0.5 * (evd.mean() - threshold) * ((evd.mean() - threshold)**2/evd.var()+1)
+            shape = -0.5 * ((evd.mean()-threshold)**2/(evd.var())-1)
 
-        #Validity of MOM
-        if shape < 1/4:
-            cov = (1-shape)**2/(1-3*shape)/(1-4*shape)/len(evd) * np.array([[2*scale**2*(1-6*shape+12*shape**2)/(1-2*shape), scale*(1-4*shape+12*shape**2)], [scale*(1-4*shape+12*shape**2), (1-2*shape)*(1-shape+6*shape**2)]])
-            fmle = multivariate_normal([scale, shape], cov.tolist()) 
-        
-        #Validity of MLE
-        else:
-            (shape, threshold, scale) = genpareto.fit(evd, floc = row['t'])
-            cov = (1+shape)/len(evd)*np.array([[2*scale**2, scale], [scale, (1+shape)]])
-            fmle = multivariate_normal([scale, shape], cov.tolist()) 
+            #Validity of MOM
+            if shape < 1/4:
+                cov = (1-shape)**2/(1-3*shape)/(1-4*shape)/len(evd) * np.array([[2*scale**2*(1-6*shape+12*shape**2)/(1-2*shape), scale*(1-4*shape+12*shape**2)], [scale*(1-4*shape+12*shape**2), (1-2*shape)*(1-shape+6*shape**2)]])
+                fmle = multivariate_normal([scale, shape], cov.tolist()) 
+            
+            #Validity of MLE
+            else:
+                (shape, threshold, scale) = genpareto.fit(evd, floc = row['Threshold (um)'])
+                cov = (1+shape)/len(evd)*np.array([[2*scale**2, scale], [scale, (1+shape)]])
+                fmle = multivariate_normal([scale, shape], cov.tolist()) 
 
-            #If neither work (could be due to inconsistent fit)
-            if shape > -0.5:
-                print('Error Shape Out Of Bounds')
-                print(shape)
+                #If neither work (could be due to inconsistent fit)
+                if shape > -0.5:
+                    print('Error Shape Out Of Bounds')
+                    print(shape)
 
-        #Mean of MLE and Variance of poisson dist divided by vol
-        #https://stats.stackexchange.com/questions/549204/variance-of-mle-poisson-distribution
-        #https://www.statlect.com/fundamentals-of-statistics/Poisson-distribution-maximum-likelihood
-        stats = (len(evd)/row['Volume (mm^3)']*v1, np.sqrt(len(evd)/row['Volume (mm^3)']**2) *v1)
-        
-        df.iloc[i]['Shape'] = shape
-        df.iloc[i]['Scale'] = scale
-        df.iloc[i]['STDShape'] = cov[1,1]
-        df.iloc[i]['STDScale'] = cov[0,0]
-        df.iloc[i]['CovShapeScale'] = cov[1,0]
-        df.iloc[i]['Rate (1/mm^3)'] = stats[0]
-        df.iloc[i]['STDRate'] = stats[1]
-        df.to_csv('parameters_new.csv')
+            #Mean of MLE and Variance of poisson dist divided by vol
+            #https://stats.stackexchange.com/questions/549204/variance-of-mle-poisson-distribution
+            #https://www.statlect.com/fundamentals-of-statistics/Poisson-distribution-maximum-likelihood
+            stats = (len(evd)/row['Volume (mm^3)'], np.sqrt(len(evd)/row['Volume (mm^3)']**2))
+            
+            df.iloc[i]['Shape'] = shape
+            df.iloc[i]['Scale'] = scale
+            df.iloc[i]['STDShape'] = cov[1,1]
+            df.iloc[i]['STDScale'] = cov[0,0]
+            df.iloc[i]['CovShapeScale'] = cov[1,0]
+            df.iloc[i]['Rate (1/mm^3)'] = stats[0]
+            df.iloc[i]['STDRate'] = stats[1]
+            df.to_csv('parameters_new.csv')
 
-        #get poisson distribution with gaussian estimator        
-        fn = getfn(stats)
-        xs = np.arange(0, 3*stats[0])
+            #get poisson distribution with gaussian estimator        
+            fn = getfn(stats*v1)
+            xs = np.arange(0, 3*stats[0]*v1)
 
-        probs = []
+            probs = []
 
-        #get probabilities for all likely values of x number of pores
-        for j in range(len(xs)):
-            try:
-                probs.append(N(fn.subs(X, xs[j])))
-            except Exception:
-                probs.append(0)
-        probs = np.array(probs, dtype = float)
+            #get probabilities for all likely values of x number of pores
+            for j in range(len(xs)):
+                try:
+                    probs.append(N(fn.subs(X, xs[j])))
+                except Exception:
+                    probs.append(0)
+            probs = np.array(probs, dtype = float)
 
-        
-        #get values of pore size
-        xs = np.arange(threshold, np.max(evd)+threshold, dx)
-        fx = F_x(xs, stats[0])
+            
+            #get values of pore size
+            xs = np.arange(threshold, np.max(evd)+threshold, dx)
+            fx = F_x(xs, stats[0]*v1)
 
-        #obtain standard cdf values of pore size cdf
-        ys = (np.ones_like(xs)-(np.ones_like(xs)+shape/scale*(xs - threshold*np.ones_like(threshold)))**(-1/shape))**stats[0]
+            #obtain standard cdf values of pore size cdf
+            ys = (np.ones_like(xs)-(np.ones_like(xs)+shape/scale*(xs - threshold*np.ones_like(threshold)))**(-1/shape))**stats[0]*v1
 
-        xs = cp.array(np.arange(0, 3*stats[0]))
-        cdf = cp.array(np.array(np.cumsum(probs*dx), dtype = float))
+            xs = cp.array(np.arange(0, 3*stats[0]*v1))
+            cdf = cp.array(np.array(np.cumsum(probs*dx), dtype = float))
 
-        #inverse CDF method
-        sampler = uniform()
+            #inverse CDF method
+            sampler = uniform()
 
-        #generate random variables using inverse cdf method
-        samples = cp.array(sampler.rvs(1000))
-
-
-        pgs = cp.zeros_like(samples)
-
-        #generate 1000 pore numbers
-        for i in range(len(pgs)):
-            try:
-                pgs[i] = xs[cp.max(np.argwhere(cdf < samples[i]))]
-            except Exception:
-                if samples[i]  > 0.5:
-                    pgs[i] = xs[-1]
-                else:
-                    pgs[i] = xs[0]
+            #generate random variables using inverse cdf method
+            samples = cp.array(sampler.rvs(1000))
 
 
-        #generate 1000 shape scale parameters
-        ss = cp.array(fmle.rvs(1000))
+            pgs = cp.zeros_like(samples)
+
+            #generate 1000 pore numbers
+            for i in range(len(pgs)):
+                try:
+                    pgs[i] = xs[cp.max(np.argwhere(cdf < samples[i]))]
+                except Exception:
+                    if samples[i]  > 0.5:
+                        pgs[i] = xs[-1]
+                    else:
+                        pgs[i] = xs[0]
 
 
-        #generate inverse CDF p values
-        samples = cp.array(sampler.rvs(1000))
-
-        xs = cp.zeros(len(samples)*len(pgs)*len(ss))
-        l = 0
-
-        t1 = ss[:, 0]/ss[:, 1]
-        s0  = 1/pgs
-        ones = cp.ones_like(samples)
-        th = ones*threshold
-
-        #compute maximum pore size from all of these rvs
-        for i in tqdm(range(len(pgs))):
-            for j in range(len(ss)):
-                xs[l:l+len(samples)] = t1[j]*((ones - samples**s0)**(-ss[j,1])-ones) + th
-                l+=len(samples)
+            #generate 1000 shape scale parameters
+            ss = cp.array(fmle.rvs(1000))
 
 
-        #generation of CDFs
-        samples = xs.get()
-        xs = np.arange(threshold, np.max(evd)+threshold, dx)
-        xs2 = np.arange(threshold, np.quantile(samples, 0.99), dx)
-        pdf, bins = np.histogram(samples, xs2, density = True)
-        cdf = np.cumsum(pdf*dx)
+            #generate inverse CDF p values
+            samples = cp.array(sampler.rvs(1000))
 
-        bins = (bins[1:] + bins[:-1])/2
+            xs = cp.zeros(len(samples)*len(pgs)*len(ss))
+            l = 0
 
-        #saving cdf
-        np.save('CDFs/' + row.Name + '_CDF_{}.npy'.format(v1), np.array([bins, cdf]))
+            t1 = ss[:, 0]/ss[:, 1]
+            s0  = 1/pgs
+            ones = cp.ones_like(samples)
+            th = ones*threshold
+
+            #compute maximum pore size from all of these rvs
+            for i in tqdm(range(len(pgs))):
+                for j in range(len(ss)):
+                    xs[l:l+len(samples)] = t1[j]*((ones - samples**s0)**(-ss[j,1])-ones) + th
+                    l+=len(samples)
+
+
+            #generation of CDFs
+            samples = xs.get()
+            xs = np.arange(threshold, np.max(evd)+threshold, dx)
+            xs2 = np.arange(threshold, np.quantile(samples, 0.99), dx)
+            pdf, bins = np.histogram(samples, xs2, density = True)
+            cdf = np.cumsum(pdf*dx)
+
+            bins = (bins[1:] + bins[:-1])/2
+
+            #saving cdf
+            np.save('CDFs/' + row.Name + '_CDF_{}.npy'.format(v1), np.array([bins, cdf]))
